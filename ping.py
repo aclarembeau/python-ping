82b985655d4910e7bb7ea8eb753efddec7f320d7
Alexis Clarembeau <alexis.clarembeau@gmail.com>
2017-03-23 16:10:13 +0100

    Adding code

---
+++ ping.py
@@ -0,0 +1,45 @@
+import subprocess 
+
+def ping(destination, ip_version=4, num_tries=1, timeout=1, encoding="utf-8"):
+    '''
+        Performs a ping to 'destination', with version 'ip_version' of IP
+        Tries 'num_tries' times, with a timeout of 'timeout'. 
+        Extracts the result with 'encoding' 
+
+        Returns: 
+            [packet, rtt, error]
+            packet = transmitted, received, loss, time, time unit
+            rtt = min, max, avg, mdev, time unit
+    '''
+
+    # Defining command
+    if ip_version == 4: 
+        command = "ping {} -c {} -W {}".format(destination, num_tries, timeout)
+    else: 
+        command = "ping6 {} -c {} -W {}".format(destination, num_tries, timeout)
+
+    # Executing command
+    p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    p_out, p_err = p.communicate()
+    p_code = p.returncode
+    p_out, p_err = p_out.decode(encoding), p_err.decode(encoding)
+
+    # Formatting output
+    error = "" 
+    rtt = [0, 0, 0, 0, '']
+    packets = [0, 0, 0, 0]
+
+    if len(p_err) > 0: 
+        error = p_err
+    else: 
+        lines = p_out.split("\n") 
+        rtt_line = (lines[-2]).split(' ') 
+        rtt = rtt_line[-2].split('/') + [rtt_line[-1]]
+        pack_line = (lines[-3]).split(' ')
+        packets = [pack_line[0], pack_line[3], pack_line[5], pack_line[-1]]
+
+
+    return [packets, rtt, error]
+
+destination = input("Destination? ") 
+print(ping(destination))
